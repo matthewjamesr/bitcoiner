e825928387f175b44100ff6cf679dd38fe2efcee
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 16:17:01 -0500

    Fixed else statement

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ app.post('/webhook/', function (req, res) {
 						data = JSON.parse(response.body)
 						sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data["USD"]["15m"])
 				  }
-					sendTextMessage(sender, "Sorry, we could not find an updated price for you.")
+					else {
+						sendTextMessage(sender, "Sorry, we could not find an updated price for you.")
+					}
 				})
 				continue
 			}
