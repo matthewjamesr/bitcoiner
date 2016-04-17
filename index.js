d665cf100349b3ec9a9bf50d27c3f7ee5fc30b99
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 15:19:40 -0500

    Fix

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.post('/webhook/', function (req, res) {
 			}
 
 			if (findWord('Price', text) == true || findWord('Rate', text) == true && findWord('Current', text) == true) {
-				data = exchange.getTicker
-				console.log(data["USD"]["15m"])
-				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data['USD']["15m"])
+				data = exchange.getTicker["USD"]
+				console.log(data["15m"])
+				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data["15m"])
 				continue
 			}
 
