6ed0c112a4823b446c3f088217b63858d95e31a9
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 16:33:16 -0500

    Push to Github

---
+++ index.js
@@ -0,0 +1,149 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+var blockchain = require('blockchain.info')
+var http = require('http-request')
+
+app.set('port', (process.env.PORT || 5000))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// index
+app.get('/', function (req, res) {
+	res.send('hello world i am a secret bot')
+})
+
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// to post data
+app.post('/webhook/', function (req, res) {
+	messaging_events = req.body.entry[0].messaging
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i]
+		sender = event.sender.id
+		if (event.message && event.message.text) {
+			text = event.message.text
+
+			// Listen for key words.
+
+			if (findWord('Generic', text) == true ) {
+				sendGenericMessage(sender)
+				continue
+			}
+
+			if (findWord('Price', text) == true || findWord('Rate', text) == true && findWord('Current', text) == true) {
+				request('https://blockchain.info/ticker', function (error, response, body) {
+				  if (!error && response.statusCode == 200) {
+				    console.log(response.body.toString())
+						data = JSON.parse(response.body)
+						sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data["USD"]["15m"])
+				  }
+					else {
+						sendTextMessage(sender, "Sorry, we could not find an updated price for you.")
+					}
+				})
+				continue
+			}
+
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
+		}
+	}
+	res.sendStatus(200)
+})
+
+var token = "CAAJoGj44QCMBAPoPnJzZBfLiWXpgShOVR0CVN86orScTpXVyG7FDXY6pP3qRR0HGZA6qdavZCBzQG2tcI0Ro1tMgnwgC1ET2dcZBk6sqe57EiYZC1l5cErvp32gXZBL8FQgP2w2fEGnP6ZCybrPo7lXTZCuDrtmCn74ZBJJgfLeGAVVahW9AdB5rIsrjFvzC6nkHGxQbGHZCZCOkwZDZD"
+
+function findWord(word, str) {
+  return RegExp('\\b'+ word +'\\b', 'i').test(str)
+}
+
+function sendTextMessage(sender, text) {
+	messageData = {
+		text:text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendGenericMessage(sender) {
+	messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+// spin spin sugar
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
