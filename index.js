99ecbcb761bb25eb1b75049b14b2fa0e77779a32
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 14:53:11 -0500

    Parse Errors

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/webhook/', function (req, res) {
 			}
 
 			if (findWord('Price', text) == true || findWord('Rate', text) == true && findWord('Current', text) == true) {
-				var data = JSON.parse(exchange.getTicker())
-				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data)
+				var data = exchange.getTicker()
+				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data['USD'])
 				continue
 			}
 
