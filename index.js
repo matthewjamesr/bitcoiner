891965b804ea37f942e812a67b33e1bea88d9d73
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 14:42:11 -0500

    Variable changed

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
 
 			if (findWord('Price', text) == true || findWord('Rate', text) == true && findWord('Current', text) == true) {
 				data = exchange.getTicker()
-				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + this.data["USD"])
+				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data)
 				continue
 			}
 
