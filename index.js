685431a915f88845725dc61394c3c01763fd464b
Matthew Reichardt <me@matthewreichardt.com>
2016-04-17 15:02:47 -0500

    Add missing bracket

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
 			}
 
 			if (findWord('Price', text) == true || findWord('Rate', text) == true && findWord('Current', text) == true) {
-				var data = exchange.getTicker
+				var data = exchange.getTicker()
 				console.log(data["USD"])
 				sendTextMessage(sender, "The current exchange rate for 1 XBTC is $" + data['USD'])
 				continue
